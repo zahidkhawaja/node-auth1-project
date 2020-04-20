81d0599d3267665ff3b469e49aeae8db7a2e2041
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-20 15:39:22 -0400

    Finalize endpoints, verify functionality

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,6 +20,7 @@ router.post("/register", (req, res) => {
       });
   });
 
+  // POST login user endpoint
   router.post("/login", (req, res) => {
     let { username, password } = req.body;
     Users.findBy({ username })
--- migrations/20200420151332_users.js
+++ migrations/20200420151332_users.js
@@ -9,6 +9,6 @@ exports.up = function(knex) {
     });
   };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
     return knex.schema.dropTableIfExists('users');
   };
